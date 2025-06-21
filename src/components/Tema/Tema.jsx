70f83fe9c19083b927ebcd780e4aa2196868666b
Ronddaa <tronddaa@gmail.com>
2025-06-21 17:30:23 +0200

    end tickets

--- src/components/Tema/Tema.jsx
+++ src/components/Tema/Tema.jsx
@@ -8,63 +8,39 @@ import "swiper/css/effect-fade";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
-import { Mousewheel, EffectFade } from "swiper/modules";
+import { Mousewheel } from "swiper/modules";
 
 export default function Tema() {
   return (
     <section className={styles.sectionTema}>
+      <p className={styles.textSection}>(теми конференції)</p>
+      <ul className={styles.wrapperTitles}>
+        <li>
+          <h2 className={styles.defoultStylestitle}>Стан</h2>
+        </li>
+        <li>
+          <h2 className={styles.defoultStylestitle}>тіло</h2>
+        </li>
+        <li>
+          <h2 className={styles.defoultStylestitle}>гроші</h2>
+        </li>
+        <li>
+          <h2 className={styles.defoultStylestitle}>секс</h2>
+        </li>
+      </ul>
       <Swiper
-        spaceBetween={30}
+        spaceBetween={0}
         direction="vertical"
-        effect={"fade"}
-        modules={[EffectFade, Mousewheel]}
+        modules={[Mousewheel]}
         mousewheel={{
           releaseOnEdges: true,
         }}
         className="mySwiper"
       >
-        <SwiperSlide className={styles.swiperTema1}>
-          <p className={styles.textSection}>(теми конференції)</p>
-          <h2 className={`${styles.titleTema1} ${styles.titleTema1Slide1}`}>
-            Стан
-          </h2>
-          <h2 className={styles.titleTema1}>
-            Тіло<span className={styles.titleTemaSpan}>(генетика)</span>
-          </h2>
-          <h2 className={styles.titleTema1}>Гроші</h2>
-          <h2 className={styles.titleTema1}>Секс</h2>
-        </SwiperSlide>
-        <SwiperSlide className={styles.swiperTema2}>
-          <p className={styles.textSection}>(теми конференції)</p>
-          <h2 className={styles.titleTema2}>Стан</h2>
-          <h2 className={`${styles.titleTema2} ${styles.titleTema2Slide2}`}>
-            Тіло<span className={styles.titleTemaSpan}>(генетика)</span>
-          </h2>
-          <h2 className={styles.titleTema2}>Гроші</h2>
-          <h2 className={styles.titleTema2}>Секс</h2>
-        </SwiperSlide>
-        <SwiperSlide className={styles.swiperTema3}>
-          <p className={styles.textSection}>(теми конференції)</p>
-          <h2 className={styles.titleTema3}>Стан</h2>
-          <h2 className={styles.titleTema3}>
-            Тіло<span className={styles.titleTemaSpan}>(генетика)</span>
-          </h2>
-          <h2 className={`${styles.titleTema3} ${styles.titleTema2Slide3}`}>
-            Гроші
-          </h2>
-          <h2 className={styles.titleTema3}>Секс</h2>
-        </SwiperSlide>
-        <SwiperSlide className={styles.swiperTema4}>
-          <p className={styles.textSection}>(теми конференції)</p>
-          <h2 className={styles.titleTema4}>Стан</h2>
-          <h2 className={styles.titleTema4}>
-            Тіло<span className={styles.titleTemaSpan}>(генетика)</span>
-          </h2>
-          <h2 className={styles.titleTema4}>Гроші</h2>
-          <h2 className={`${styles.titleTema4} ${styles.titleTema2Slide4}`}>
-            Секс
-          </h2>
-        </SwiperSlide>
+        <SwiperSlide className={styles.swiperTema1}></SwiperSlide>
+        <SwiperSlide className={styles.swiperTema2}></SwiperSlide>
+        <SwiperSlide className={styles.swiperTema3}></SwiperSlide>
+        <SwiperSlide className={styles.swiperTema4}></SwiperSlide>
       </Swiper>
     </section>
   );
--- src/components/TicketsSection/TicketsSection.jsx
+++ src/components/TicketsSection/TicketsSection.jsx
@@ -66,21 +66,25 @@ export default function Tickets() {
                 <svg className={styles.detailsSVG} width={28} height={28}>
                   <use xlinkHref={`${sprite}#icon-diamond`}></use>
                 </svg>
-                <p className={styles.detailsText}>Місця: позначені жовтим</p>
+                <p className={styles.detailsText}>
+                  Місця: <span>ряди зазначені стендом Balance</span>
+                </p>
               </li>
               <li>
                 <svg className={styles.detailsSVG} width={28} height={28}>
                   <use xlinkHref={`${sprite}#icon-palms`}></use>
                 </svg>
                 <p className={styles.detailsText}>
-                  Напої: один келих ігристого + вода
+                  Напої: один келих <br /> ігристого + вода
                 </p>
               </li>
               <li>
                 <svg className={styles.detailsSVG} width={28} height={28}>
                   <use xlinkHref={`${sprite}#icon-clock`}></use>
                 </svg>
-                <p className={styles.detailsText}>Дві кавопаузи</p>
+                <p className={styles.detailsText}>
+                  Дві кавопаузи <br /> <span>+ Легкий перекус</span>
+                </p>
               </li>
               <li>
                 <svg className={styles.detailsSVG} width={28} height={28}>
@@ -109,7 +113,7 @@ export default function Tickets() {
                   <use xlinkHref={`${sprite}#icon-coins`}></use>
                 </svg>
                 <p className={styles.detailsText}>
-                  Ексклюзивний концерт MONATIK
+                  <span>Пакет з подарунками</span>
                 </p>
               </li>
               <li>
@@ -117,49 +121,57 @@ export default function Tickets() {
                   <use xlinkHref={`${sprite}#icon-micro`}></use>
                 </svg>
                 <p className={styles.detailsText}>
-                  Ексклюзивний концерт MONATIK
+                  <span>-20% знижки на наступну конференцію</span>
                 </p>
               </li>
             </ul>
-            <p className={styles.priceText}></p>
-            <button className={styles.btnBuyTariff}>придбати квиток</button>
+            <p className={styles.priceText}>450$</p>
+            <button className={styles.btnBuyTariffPremium}>
+              придбати квиток
+            </button>
             <button className={styles.btnPlaceShow}>переглянути місця</button>
           </li>
-          <li>
-            <h3 className={styles.titleTariffCard}></h3>
+          <li className={styles.TicketsGold}>
+            <h3 className={styles.titleTariffCard}>LUXE</h3>
             <p className={styles.kolPlace}>Залишилось 30 місць</p>
             <ul className={styles.wrapperDetailsTariff}>
               <li>
                 <svg className={styles.detailsSVG} width={28} height={28}>
                   <use xlinkHref={`${sprite}#icon-diamond`}></use>
                 </svg>
-                <p className={styles.detailsText}>Місця: позначені жовтим</p>
+                <p className={styles.detailsText}>
+                  Місця: <span>перший ряд</span>
+                </p>
               </li>
               <li>
                 <svg className={styles.detailsSVG} width={28} height={28}>
                   <use xlinkHref={`${sprite}#icon-palms`}></use>
                 </svg>
                 <p className={styles.detailsText}>
-                  Напої: один келих ігристого + вода
+                  Напої: <span>безлімітне ігристе</span> <br />+ вода
                 </p>
               </li>
               <li>
                 <svg className={styles.detailsSVG} width={28} height={28}>
                   <use xlinkHref={`${sprite}#icon-clock`}></use>
                 </svg>
-                <p className={styles.detailsText}>Дві кавопаузи</p>
+                <p className={styles.detailsText}>
+                  Дві кавопаузи <br />+ Легкий перекус
+                </p>
               </li>
               <li>
                 <svg className={styles.detailsSVG} width={28} height={28}>
                   <use xlinkHref={`${sprite}#icon-hands`}></use>
                 </svg>
-                <p className={styles.detailsText}>Дві кавопаузи</p>
+                <p className={styles.detailsText}>
+                  <span>Повноцінний обід зі спікерами</span>
+                </p>
               </li>
               <li>
                 <svg className={styles.detailsSVG} width={28} height={28}>
                   <use xlinkHref={`${sprite}#icon-people`}></use>
                 </svg>
-                <p className={styles.detailsText}>Нетворкінг </p>
+                <p className={styles.detailsText}>Нетворкінг</p>
               </li>
               <li>
                 <svg className={styles.detailsSVG} width={28} height={28}>
@@ -173,16 +185,14 @@ export default function Tickets() {
                 <svg className={styles.detailsSVG} width={28} height={28}>
                   <use xlinkHref={`${sprite}#icon-pazzle`}></use>
                 </svg>
-                <p className={styles.detailsText}>
-                  Ексклюзивний концерт MONATIK
-                </p>
+                <p className={styles.detailsText}>Пакет з подарунками</p>
               </li>
               <li>
                 <svg className={styles.detailsSVG} width={28} height={28}>
                   <use xlinkHref={`${sprite}#icon-star`}></use>
                 </svg>
                 <p className={styles.detailsText}>
-                  Ексклюзивний концерт MONATIK
+                  <span>Бранч в президенському номері </span>
                 </p>
               </li>
               <li>
@@ -190,7 +200,7 @@ export default function Tickets() {
                   <use xlinkHref={`${sprite}#icon-convert`}></use>
                 </svg>
                 <p className={styles.detailsText}>
-                  Ексклюзивний концерт MONATIK
+                  <span>Бранч в президенському номері </span>
                 </p>
               </li>
               <li>
@@ -198,7 +208,7 @@ export default function Tickets() {
                   <use xlinkHref={`${sprite}#icon-coins`}></use>
                 </svg>
                 <p className={styles.detailsText}>
-                  Ексклюзивний концерт MONATIK
+                  -20% знижки на наступну конференцію
                 </p>
               </li>
               <li>
@@ -206,14 +216,37 @@ export default function Tickets() {
                   <use xlinkHref={`${sprite}#icon-micro`}></use>
                 </svg>
                 <p className={styles.detailsText}>
-                  Ексклюзивний концерт MONATIK
+                  Ексклюзивний <br /> концерт MONATIK
                 </p>
               </li>
             </ul>
-            <p className={styles.priceText}></p>
-            <button className={styles.btnBuyTariff}>придбати квиток</button>
+            <p className={styles.priceText}>1200$</p>
+            <button className={styles.btnBuyTariffGold}>придбати квиток</button>
             <button className={styles.btnPlaceShow}>переглянути місця</button>
           </li>
+          <li className={styles.TicketsOnlyMonatik}>
+            <h3 className={styles.titleOnlyTiket}>ONLY TICKET</h3>
+            <div className={styles.wrapperInfoTicketOnly}>
+              <p className={styles.kolPlace}>Залишилось 30 місць</p>
+              <p className={styles.detailsText}>
+                <svg className={styles.detailsSVG} width={50} height={50}>
+                  <use xlinkHref={`${sprite}#icon-micro`}></use>
+                </svg>
+                <span>
+                  Вхід на фінальну частину конференції — ексклюзивний концерт
+                  MONATIK
+                </span>
+              </p>
+              <p className={styles.priceText}>150$</p>
+              <button className={styles.btnBuyTariffGold}>
+                придбати квиток
+              </button>
+            </div>
+          </li>
+        </ul>
+        <ul className={styles.wrapperAuction}>
+          <li>(Аукціон речей зі змістом)</li>
+          <li>Мистецтво, створене серцем. Для тих, хто відчуває глибше</li>
         </ul>
       </div>
     </section>
