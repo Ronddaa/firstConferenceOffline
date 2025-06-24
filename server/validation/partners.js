b893b46533f077c8996dba4a465c2ed99a1e1e3e
Ronddaa <tronddaa@gmail.com>
2025-06-24 02:40:40 +0200

    del server folder

--- server/validation/partners.js
+++
@@ -1,9 +0,0 @@
-import Joi from "joi";
-
-export const createPartnerSchema = Joi.object({
-  companyName: Joi.string().required(),
-  contactPerson: Joi.string().required(),
-  contactInfo: Joi.string().required(),
-  telegramNick: Joi.string().required(),
-  instagramLink: Joi.string().required(),
-});
--- server/validation/speakers.js
+++
@@ -1,8 +0,0 @@
-import Joi from "joi";
-
-export const createSpeakerSchema = Joi.object({
-  fullName: Joi.string().required(),
-  phone: Joi.string().required(),
-  telegram: Joi.string().required(),
-  instagram: Joi.string().required(),
-});
